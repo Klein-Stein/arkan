78b9d33d9eb63f1ce6fef399a0320bff909d99d1
Denis Sologub <contedevel2010@gmail.com>
2022-06-14 17:21:28 +0300

    Add a new toolchain file, update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ A_OUTDIR=$A_BUILDDIR/out
 A_ANDROID_SDK=$ANDROID_HOME
 A_ANDROID_NDK=$ANDROID_NDK_HOME
 A_ANDROID_MIN_API_LEVEL=29
+A_THREADS=4
 
 
 # Check for .env configuration
@@ -17,24 +18,63 @@ fi
 mkdir -p $A_BUILDDIR
 mkdir -p $A_OUTDIR
 
-# Check if Android NDK was set
+###################
+# Android platform
+###################
 if [[ ! -z $A_ANDROID_NDK ]]; then
 
     # Build the library for Android
     for ABI in "arm64-v8a" "x86_64" "x86" "armeabi-v7a"; do
 
-        LOCAL_BUILDDIR=$A_BUILDDIR/android/$ABI
-        LOCAL_OUTDIR=$A_OUTDIR/android/$ABI
-        mkdir -p $LOCAL_BUILDDIR
-        mkdir -p $LOCAL_OUTDIR
+        ANDROID_BUILDDIR=$A_BUILDDIR/android/$ABI
+        ANDROID_OUTDIR=$A_OUTDIR/android/$ABI
+        mkdir -p $ANDROID_BUILDDIR
+        mkdir -p $ANDROID_OUTDIR
 
-        cmake -S . -B $LOCAL_BUILDDIR \
+        cmake -S . -B $ANDROID_BUILDDIR \
             -DCMAKE_TOOLCHAIN_FILE=$A_ANDROID_NDK/build/cmake/android.toolchain.cmake \
             -DANDROID_ABI=$ABI \
             -DANDROID_NATIVE_API_LEVEL=$A_ANDROID_MIN_API_LEVEL
 
-        cmake --build $LOCAL_BUILDDIR -j 4
-        cp $LOCAL_BUILDDIR/*.so $LOCAL_OUTDIR
+        cmake --build $ANDROID_BUILDDIR -j $A_THREADS
+        cp $ANDROID_BUILDDIR/*.so $ANDROID_OUTDIR
 
     done
 fi
+
+
+#################
+# Linux platform
+#################
+for ABI in "x86_64"; do
+
+    LINUX_BUILDDIR=$A_BUILDDIR/linux/$ABI
+    LINUX_OUTDIR=$A_OUTDIR/linux/$ABI
+    mkdir -p $LINUX_BUILDDIR
+    mkdir -p $LINUX_OUTDIR
+
+    cmake -S . -B $LINUX_BUILDDIR \
+        -DCMAKE_TOOLCHAIN_FILE=toolchains/${ABI}_linux_gnu.toolchain.cmake
+
+    cmake --build $LINUX_BUILDDIR -j $A_THREADS
+    cp $LINUX_BUILDDIR/*.so $LINUX_OUTDIR
+
+done
+
+###################
+# Windows platform
+###################
+for ABI in "x86_64"; do
+
+    WINDOWS_BUILDDIR=$A_BUILDDIR/windows/$ABI
+    WINDOWS_OUTDIR=$A_OUTDIR/windows/$ABI
+    mkdir -p $LINUX_BUILDDIR
+    mkdir -p $LINUX_OUTDIR
+
+    cmake -S . -B $WINDOWS_BUILDDIR \
+        -DCMAKE_TOOLCHAIN_FILE=toolchains/${ABI}_windows_gnu.toolchain.cmake
+
+    cmake --build $WINDOWS_BUILDDIR -j $A_THREADS
+    cp $WINDOWS_BUILDDIR/*.so $WINDOWS_OUTDIR
+
+done
---
+++ toolchains/x86_64_linux_gnu.toolchain.cmake
@@ -0,0 +1,35 @@
+set(triple  x86_64-linux-gnu)
+set(llvm    /opt/homebrew/Cellar/llvm/13.0.1_1)
+
+set(CMAKE_SYSTEM_NAME       Linux)
+set(CMAKE_SYSTEM_PROCESSOR  x86_64)
+
+set(CMAKE_IGNORE_PATH                       /Applications/Xcode.app)
+set(CMAKE_PREFIX_PATH                       ${llvm})
+
+set(CMAKE_C_COMPILER                        ${llvm}/bin/clang)
+set(CMAKE_C_COMPILER_TARGET                 ${triple})
+set(CMAKE_C_STANDARD_LIBRARIES              ${llvm}/lib)
+set(CMAKE_C_STANDARD_INCLUDE_DIRECTORIES    ${llvm}/include/c++/v1)
+set(CMAKE_C_LINK_EXECUTABLE                 "${llvm}/bin/llvm-link <FLAGS> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+
+set(CMAKE_CXX_COMPILER                      ${llvm}/bin/clang++)
+set(CMAKE_CXX_COMPILER_TARGET               ${triple})
+set(CMAKE_CXX_STANDARD_LIBRARIES            ${llvm}/lib)
+set(CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES  ${llvm}/include/clang)
+set(CMAKE_CXX_LINK_EXECUTABLE               "${llvm}/bin/llvm-link <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+
+# set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+# set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+# set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
+
+# set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+# set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+# set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+SET(CMAKE_AR      ${llvm}/bin/llvm-ar)
+SET(CMAKE_LINKER  ${llvm}/bin/llvm-link)
+SET(CMAKE_NM      ${llvm}/bin/llvm-nm)
+SET(CMAKE_OBJDUMP ${llvm}/bin/llvm-objdump)
+SET(CMAKE_RANLIB  ${llvm}/bin/llvm-ranlib)
